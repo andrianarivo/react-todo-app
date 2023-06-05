6ebe50bec7926ce1a9cb3d387291fb8f328b54e7
andrianarivo <andrianarivo.david@gmail.com>
2023-06-05 16:11:48 +0300

    setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TodoApp from "@/components/TodoApp.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>,
+)
