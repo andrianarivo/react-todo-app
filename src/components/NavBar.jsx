3a931a07eccfc805ca16e44a75403da57c6772c3
Andrianarivo David <andrianarivo.david@gmail.com>
2023-06-05 21:15:30 +0300

    gh-pages deployment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,93 @@
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '@/context/AuthContext.jsx';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { FiMenu } from 'react-icons/fi';
+import { MdClose } from 'react-icons/md';
 
 const Navbar = () => {
   const { user, logout } = useAuthContext();
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (navbarOpen && ref.current && !ref.current.contains(event.target)) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [navbarOpen]);
+
   const handleLogout = () => {
     logout();
     navigate('/login');
   };
+
   const links = [
     { path: '/', text: 'Home' },
     { path: 'about', text: 'About' },
     { path: 'profile', text: 'Profile' },
     { path: 'login', text: 'Login' },
   ];
+
   return (
     <>
-      <nav className="navbar">
-        <ul>
+      <nav ref={ref} className="navbar">
+        <button
+          className="toggle"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        >
+          {navbarOpen ? (
+            <MdClose style={{ width: '32px', height: '32px' }} />
+          ) : (
+            <FiMenu
+              style={{
+                width: '32px',
+                height: '32px',
+              }}
+            />
+          )}
+        </button>
+        <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}>
           {links.map((link) => {
             return (
               <React.Fragment key={link.text}>
                 {link.path === 'login' ? (
                   !user && (
                     <li>
-                      <NavLink to={link.path}>{link.text}</NavLink>
+                      <NavLink
+                        to={link.path}
+                        onClick={() => setNavbarOpen(false)}
+                      >
+                        {link.text}
+                      </NavLink>
                     </li>
                   )
                 ) : link.path === 'profile' ? (
                   user && (
                     <li>
-                      <NavLink to={link.path}>{link.text}</NavLink>
+                      <NavLink
+                        to={link.path}
+                        onClick={() => setNavbarOpen(false)}
+                      >
+                        {link.text}
+                      </NavLink>
                     </li>
                   )
                 ) : (
                   <li>
-                    <NavLink to={link.path}>{link.text}</NavLink>
+                    <NavLink
+                      to={link.path}
+                      onClick={() => setNavbarOpen(false)}
+                    >
+                      {link.text}
+                    </NavLink>
                   </li>
                 )}
               </React.Fragment>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/styles/app.css';
 import TodoApp from '@/components/TodoApp.jsx';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-    <TodoApp />
-      </Router>
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoApp />
+    </Router>
   </React.StrictMode>
 );
